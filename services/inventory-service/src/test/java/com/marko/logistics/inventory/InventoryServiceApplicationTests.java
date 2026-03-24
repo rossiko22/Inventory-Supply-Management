c0764d137e06a162f04d5348f8302720b2347e2b
rossiko22 <markospasovskq@gmail.com>
2026-03-24 04:16:00 +0100

    Microservices 2

--- services/inventory-service/src/test/java/com/marko/logistics/inventory/InventoryServiceApplicationTests.java
+++ services/inventory-service/src/test/java/com/marko/logistics/inventory/InventoryServiceApplicationTests.java
@@ -2,8 +2,18 @@ package com.marko.logistics.inventory;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(properties = {
+        "spring.datasource.url=jdbc:h2:mem:testdb;DB_CLOSE_DELAY=-1",
+        "spring.datasource.driver-class-name=org.h2.Driver",
+        "spring.datasource.username=sa",
+        "spring.datasource.password=",
+        "spring.jpa.hibernate.ddl-auto=create-drop",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "grpc.server.port=-1"
+})
 class InventoryServiceApplicationTests {
 
     @Test
